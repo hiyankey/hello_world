81180ce1fb917eb02206123d1d2a993f7a900b20
Emmanuel Yankey <hiyankey@gmail.com>
2024-12-26 14:03:32 +0000

    wip

--- main.rs
+++ main.rs
@@ -7,6 +7,25 @@ fn main(){
 
 fn test_func(){
 
-    let x:i32 = 5;
-    println!("{}", x)
+    let x:f32 = 225.0;
+    let y:u8 = x as u8 -5;
+    println!("{:?}", y);
+
+    let mut is_ready: bool = true;
+    println!("{}",is_ready);
+    is_ready = false;
+
+    println!("{}",is_ready);
+let char: char = '🔥';
+    let str: &str = "Hello dev!";
+    println!("{}", str);
+    println!("{}", char);
+
+
+    let point: (i32, u32) = (-1,4);
+println!("{:?}", point);
+
+let arr: [i16; 3] = [12, 45, 34];
+
+println!("{:?}", arr)
 }
